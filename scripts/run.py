d3637ed42f61528916c3f18de78a1e69a6ca3288
leah <leah@cnvrg.io>
2018-10-13 19:49:53 +0300

    imports example

---
+++ scripts/run.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('.')
+
+from utils.files import print_content
+print_content(5)
+print("import successfully")
---
+++ utils/files.py
@@ -0,0 +1,6 @@
+import os
+import time
+
+def print_content(sleep):
+    time.sleep(sleep)
+    print("sleeping for %s" % sleep)
